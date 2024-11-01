4884294e627e80e82b61c6ca3ed5936e9c1b7fc0
ImrevdV <imre.vdvoort@gmail.com>
2024-11-01 16:35:25 +0100

    basic setup / conditional rendering(listings)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Listing::create([
+            'title' => 'lamp',
+            'tags' => 'licht, meubel',
+            'description' => 'wss de mooiste lamp'
+        ]);
+
+        Listing::create([
+            'title' => 'computermuis',
+            'tags' => 'computer, elektronica',
+            'description' => 'wss de mooiste muis'
+        ]);
     }
 }
